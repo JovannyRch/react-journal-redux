26c3afab217db0ad35cd018cc33cc61dd0da380b
JovannyRch <43225215+JovannyRch@users.noreply.github.com>
2020-07-16 12:51:20 -0500

    Login y registro

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegisterPage = () => {
     return (
-        <div>
-            <h1>Register Page</h1>
-        </div>
+        <>
+            <h1 className="auth__title">Sing Up</h1>
+            <form>
+
+                <input
+                    type="text"
+                    placeholder="Correo"
+                    name="name"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+
+                <input
+                    type="email"
+                    placeholder="Correo"
+                    name="email"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+
+
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+
+                <input
+                    type="password"
+                    placeholder="Confirma tu password"
+                    name="password2"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+
+                <button type="submit" className="btn btn-primary btn-block">
+                    Registrarse
+                </button>
+                <hr />
+
+                <br />
+                <Link to="/auth/login" className="link" >
+                    Iniciar sesión
+                </Link>
+            </form>
+        </>
     )
 }
